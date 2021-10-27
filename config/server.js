1064135e76e6cf3d0b6b298a665f63d8ee553457
Brett Larry Onions <46473797+brett-onions@users.noreply.github.com>
2021-10-27 12:49:06 +0200

    changed envs

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env('STRAPI_HOST', '0.0.0.0'),
+  port: env.int('STRAPI_PORT', 1337),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'a86dbe98aafa729841abf6e1810f4e80'),
