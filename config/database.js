6d7597dc21439ef79f38cc31825aaf71d8e98754
Brett Larry Onions <46473797+brett-onions@users.noreply.github.com>
2021-10-27 10:29:21 +0200

    using mongodb as default database

--- config/database.js
+++ config/database.js
@@ -2,13 +2,18 @@ module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: 'mongoose',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        host: env('DATABASE_HOST', 'cluster0.x6nl8.mongodb.net'),
+        srv: env.bool('DATABASE_SRV', true),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', 'strapi'),
+        username: env('DATABASE_USERNAME', 'kuunika'),
+        password: env('DATABASE_PASSWORD', 'simple_easy_and_sweet2020-2021'),
       },
       options: {
-        useNullAsDefault: true,
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+        ssl: env.bool('DATABASE_SSL', true),
       },
     },
   },
